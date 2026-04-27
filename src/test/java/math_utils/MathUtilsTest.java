1f94115cb6bf19151ca5443c58ba01f8f360eb4d
qkhanh2907 <25021824@vnu.edu.vn>
2026-04-27 15:42:09 +0700

    Push lan 1: Code dang bi loi Unit Test

--- src/test/java/math_utils/MathUtilsTest.java
+++ src/test/java/math_utils/MathUtilsTest.java
@@ -8,8 +8,6 @@ public class MathUtilsTest {
     @Test
     public void testAdd() {
         MathUtils math = new MathUtils();
-        // CỐ TÌNH GÂY LỖI Ở ĐÂY ĐỂ HOÀN THÀNH YÊU CẦU ĐỀ BÀI
-        // (Sửa số 5 thành số 6 để test CI bị tạch)
-        assertEquals(5, math.add(2, 3), "2 + 3 phải bằng 5");
+        assertEquals(6, math.add(2, 3), "2 + 3 phải bằng 5");
     }
 }
